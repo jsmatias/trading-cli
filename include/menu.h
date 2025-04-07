6f8fed8b3d5ddf8b36202da0d7deb8e90eb806a6
jsmatias <smatias.jean@gmail.com>
2025-04-07 11:28:19 +0200

    first commit

---
+++ include/menu.h
@@ -0,0 +1,8 @@
+#ifndef MENU_H
+#define MENU_H
+
+void printMenu();
+int getUserChoice();
+void processChoice(int choice);
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "../include/menu.h"
+
+int main()
+{
+    int choice;
+    while (true)
+    {
+        std::cout << "\033[2J\033[1;1H";
+        std::cout << "Get ready to start trading!" << std::endl;
+        printMenu();
+        choice = getUserChoice();
+        processChoice(choice);
+        
+        std::cout << "Press Enter to continue...";
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+        std::cin.get();
+    }
+
+    return 0;
+}
